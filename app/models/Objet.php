7f3d0794c05a021bc1de3016551fcde1e893b1d6
DirkTomefy <dirktomefy@gmail.com>
2026-02-11 11:30:50 +0300

    [feat] : fonciton getAll

---
+++ app/models/Objet.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\models;
+
+use PDO;
+
+class Objet
+{
+    private PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getAllWithoutUser(int $idUser): array
+    {
+        $stmt = $this->db->prepare(
+            'SELECT o.id, o.titre, o.prix, o.detail, o.description, o.idProprio, o.idCateg, c.libele AS categorie
+             FROM objet o
+             JOIN categorie c ON c.id = o.idCateg
+             WHERE o.idProprio <> ?
+             ORDER BY o.id DESC'
+        );
+        $stmt->execute([$idUser]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
