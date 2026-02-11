305d32904a4c92cb4eb71f50551adf78e4e5e689
DirkTomefy <dirktomefy@gmail.com>
2026-02-11 11:21:13 +0300

    [fix] :sql objet fixé

--- databases/20260211-02.init_data.sql
+++ databases/20260211-02.init_data.sql
@@ -13,11 +13,11 @@ INSERT INTO user (email, login, passwd) VALUES
 
 
 -- ----- OBJETS -----
-INSERT INTO objet (titre, prix, idProprio, idCateg, description) VALUES
-('iPhone 14', 1200.00, 1, 1, 'Smartphone Apple en excellent état'),
-('T-shirt Marvel', 25.50, 2, 2, 'T-shirt neuf taille M'),
-('Le Petit Prince', 10.00, 3, 3, 'Livre classique pour enfants'),
-('MacBook Pro', 2500.00, 1, 1, 'MacBook Pro 2021, presque neuf');
+INSERT INTO objet (titre, prix, detail, idProprio, idCateg, description) VALUES
+('iPhone 14', 1200.00, '128GB - Bleu', 1, 1, 'Smartphone Apple en excellent état'),
+('T-shirt Marvel', 25.50, 'Taille M - Neuf', 2, 2, 'T-shirt neuf taille M'),
+('Le Petit Prince', 10.00, 'Édition poche', 3, 3, 'Livre classique pour enfants'),
+('MacBook Pro', 2500.00, '14 pouces - M1 Pro', 1, 1, 'MacBook Pro 2021, presque neuf');
 
 
 -- ----- IMAGE_OBJET -----
