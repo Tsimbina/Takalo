9391cdeca3c656c620454a91528d3961b82bbc91
DirkTomefy <dirktomefy@gmail.com>
2026-02-11 11:33:13 +0300

    [fix] : getAllByUser fait

--- app/models/Objet.php
+++ app/models/Objet.php
@@ -13,13 +13,13 @@ class Objet
         $this->db = $db;
     }
 
-    public function getAllWithoutUser(int $idUser): array
+    public function getAllByUser(int $idUser): array
     {
         $stmt = $this->db->prepare(
             'SELECT o.id, o.titre, o.prix, o.detail, o.description, o.idProprio, o.idCateg, c.libele AS categorie
              FROM objet o
              JOIN categorie c ON c.id = o.idCateg
-             WHERE o.idProprio <> ?
+             WHERE o.idProprio = ?
              ORDER BY o.id DESC'
         );
         $stmt->execute([$idUser]);
